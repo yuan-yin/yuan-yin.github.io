7bee653a8c131626f5b3e592922eee6f6d8ce802
yuan-yin <silveryuan.g@gmail.com>
2026-04-25 16:58:16 +0000

    Deploying to gh-pages from @ yuan-yin/yuan-yin.github.io@d6904f27ef5d21c74c3a1cd33b1eae5418b5521b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "Repositories",
-          description: "This is a list of repositories of open source implementation of my publications",
+          description: "Open-source implementations associated with my publications",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";

a9eaf68de204d976e64ab9551e9617d245416704
yuan-yin <silveryuan.g@gmail.com>
2026-08-03 21:44:40 +0200

    Update

--- assets/js/shortcut-key.js
+++
@@ -1,8 +0,0 @@
-// Mark macOS early enough that CSS can show the correct search shortcut
-// without swapping text after the navbar has already painted.
-(() => {
-  const platform = navigator.userAgentData?.platform || navigator.platform || "";
-  if (/mac/i.test(platform)) {
-    document.documentElement.classList.add("shortcut-is-mac");
-  }
-})();
